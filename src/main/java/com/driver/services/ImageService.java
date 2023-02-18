bef1b259f19b9d1c4ac2c1dad5446f49e6450d81
mayurValsangkar <vmayur1997@gmail.com>
2023-02-18 18:20:44 +0530

    solution

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -45,35 +45,22 @@ public class ImageService {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
 
         Image image = imageRepository2.findById(id).get();
-
         String dimension = image.getDimensions();
 
-        int i;
-        for(i=0;i<screenDimensions.length();i++){
-            if(screenDimensions.charAt(i)=='X'){
-                break;
-            }
-        }
+        String[] screenDimensionArray = screenDimensions.split("X");
+        String[] dimensionArray = dimension.split("X");
 
-        int l1 = Integer.valueOf(screenDimensions.substring(0, i));
-        int b1 = Integer.valueOf(screenDimensions.substring(i+1));
 
-        int totalArea = l1 * b1;
+        int l1 = Integer.valueOf(screenDimensionArray[0]);
+        int b1 = Integer.valueOf(screenDimensionArray[1]);
 
-        int j;
-        for (j=0;j<dimension.length();j++){
-            if(dimension.charAt(j)=='X'){
-                break;
-            }
-        }
+        int l2 = Integer.valueOf(dimensionArray[0]);
+        int b2 = Integer.valueOf(dimensionArray[1]);
 
-        int l2 = Integer.valueOf(dimension.substring(0, j));
-        int b2 = Integer.valueOf(dimension.substring(j+1));
+        int totalArea = l1*b1;
+        int area = l2*b2;
 
-        int area = l2 * b2;
-
-        int count = (int)Math.floor(totalArea/area);
-
+        int count = totalArea/area;
         return count;
     }
 }
